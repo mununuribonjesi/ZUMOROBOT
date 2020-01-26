de2a08c2ee870de3c09e08fbc3210801c3dc4593
Munibanks <mbonjesi@gmail.com>
2020-01-26 13:30:39 +0000

    commenting code

--- MainHeaders/TurnSensor.h
+++ MainHeaders/TurnSensor.h
@@ -1,7 +1,3 @@
-/* Turnsensor.h and TurnSensor.cpp provide functions for
-configuring the L3GD20H gyro, calibrating it, and using it to
-measure how much the robot has turned about its Z axis. */
-
 #pragma once
 
 #include <Zumo32U4.h>
